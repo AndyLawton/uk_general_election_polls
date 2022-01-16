8cffe82e1c3050542888413103e427379df84ec0
Andy <andy@sb7mobile.com>
2022-01-16 17:12:53 +0000

    Improve web_page look + Poll Update

--- html_outputs/opinion_polling.php
+++ html_outputs/opinion_polling.php
@@ -13,7 +13,8 @@
     /* Create two unequal columns that floats next to each other */
     .column {
         float: left;
-        padding: 40px;
+        width:50%;
+        padding: 10px;
         text-align: center;
     }
 
@@ -28,19 +29,43 @@
         display: table;
         clear: both;
     }
+
+    #T_polling_average_ .row0 {
+        font-weight:bold;
+        font-size:2em !important;
+    }
+
+    body {
+        max-width:1200px;
+        margin: auto;
+
+    }
 </style>
 
 <body>
 
 <div class="row">
     <div class="column">
+        <h1> Polling Average </h1>
+    </div>
+
+
+    <div class="column">
+        <br>
         <?php require("polling_average.html") ?>
-        <br/>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="column">
+        <h2> Latest Polls by Pollster </h2>
         <?php require("pollsters_recent.html") ?>
     </div>
 
 
     <div class="column">
+        <h2> Monthly Polling Average </h2>
         <?php require("monthly_averages.html") ?>
     </div>
 
@@ -53,7 +78,9 @@
     <img src="monthly_trend.png"/>
 </div>
 
-<div class="row">
+<div class="row center">
+
+    <h2> Last 25 Polls </h2>
     <?php require("top_25.html") ?>
 </div>
 
